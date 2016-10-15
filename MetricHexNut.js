8f029acc42e6b854e2659b7df6220482f575b0ce
Albert Sanchez <albert.san.iza@gmail.com>
2016-10-14 20:33:41 -0700

    Auto indent

--- MetricHexNut.js
+++ MetricHexNut.js
@@ -1,27 +1,23 @@
 //Author-Albert Sanchez
 //Description-Create a Metric Hex Nut
-
 function run(context) {
+  "use strict";
+  if (adsk.debug === true) {
+    /*jslint debug: true*/
+    debugger;
+    /*jslint debug: false*/
+  }
+  var ui;
+  try {
+    var app = adsk.core.Application.get();
+    ui = app.userInterface;
 
-    "use strict";
-    if (adsk.debug === true) {
-        /*jslint debug: true*/
-        debugger;
-        /*jslint debug: false*/
+    ui.messageBox('Hello script');
+  }
+  catch (e) {
+    if (ui) {
+      ui.messageBox('Failed : ' + (e.description ? e.description : e));
     }
- 
-    var ui;
-    try {
-        var app = adsk.core.Application.get();
-        ui = app.userInterface;
- 
-        ui.messageBox('Hello script');        
-    } 
-    catch (e) {
-        if (ui) {
-            ui.messageBox('Failed : ' + (e.description ? e.description : e));
-        }
-    }
-
-    adsk.terminate(); 
+  }
+  adsk.terminate();
 }
