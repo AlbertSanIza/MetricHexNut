6be3cb45087c916118b440afd958b8dc85c40dee
Albert Sanchez <albert.san.iza@gmail.com>
2016-10-14 20:26:54 -0700

    Added Script files

---
+++ MetricHexNut.js
@@ -0,0 +1,27 @@
+//Author-Albert Sanchez
+//Description-Create a Metric Hex Nut
+
+function run(context) {
+
+    "use strict";
+    if (adsk.debug === true) {
+        /*jslint debug: true*/
+        debugger;
+        /*jslint debug: false*/
+    }
+ 
+    var ui;
+    try {
+        var app = adsk.core.Application.get();
+        ui = app.userInterface;
+ 
+        ui.messageBox('Hello script');        
+    } 
+    catch (e) {
+        if (ui) {
+            ui.messageBox('Failed : ' + (e.description ? e.description : e));
+        }
+    }
+
+    adsk.terminate(); 
+}
