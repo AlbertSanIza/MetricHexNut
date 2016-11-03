d708fa979b81f573083cca19f28c68a158458355
Albert Sanchez <albert.san.iza@gmail.com>
2016-11-03 15:03:35 -0700

    extrude

--- MetricHexNut.js
+++ MetricHexNut.js
@@ -158,6 +158,15 @@ function run(context) {
       for(i = 0; i < 6; i++) {
         sketch.sketchCurves.sketchLines.addByTwoPoints(vertices[(i + 1) % 6], vertices[i]);
       }
+      var extrudedBody = newComp.features.extrudeFeatures;
+      var profile = sketch.profiles.item(0);
+      var extInput = extrudedBody.createInput(profile, adsk.fusion.FeatureOperations.NewBodyFeatureOperation);
+      var distance = adsk.core.ValueInput.createByReal(this.k);
+      extInput.setDistanceExtent(false, distance);
+      var headExt = extrudedBody.add(extInput);
+      var fc = headExt.faces.item(0);
+      var bd = fc.body;
+      bd.name = this.metricHexNutName;
     };
   };
   try {
