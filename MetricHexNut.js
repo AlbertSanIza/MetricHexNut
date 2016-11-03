07b9986bffc3bc447f6646182cd54e3137b1af95
Albert Sanchez <albert.san.iza@gmail.com>
2016-11-03 11:09:36 -0700

    added back some ui

--- MetricHexNut.js
+++ MetricHexNut.js
@@ -2,6 +2,37 @@
 //Description-Create a Metric Hex Nut
 function run(context) {
   "use strict";
+  var metricHexNutMatrix = [
+    {nominalSize: 'M1.6', d: 1.6, af: 3.2, k: 1.3},
+    {nominalSize: 'M2',   d: 2,   af: 4,   k: 1.6},
+    {nominalSize: 'M2.5', d: 2.5, af: 5,   k: 2},
+    {nominalSize: 'M3',   d: 3,   af: 5.5, k: 2.4},
+    {nominalSize: 'M4',   d: 4,   af: 7,   k: 3.2},
+    {nominalSize: 'M5',   d: 5,   af: 8,   k: 4.7},
+    {nominalSize: 'M6',   d: 6,   af: 10,  k: 5.2},
+    {nominalSize: 'M10',  d: 10,  af: 16,  k: 8.4},
+    {nominalSize: 'M12',  d: 12,  af: 18,  k: 10.8},
+    {nominalSize: 'M14',  d: 14,  af: 21,  k: 12.8},
+    {nominalSize: 'M16',  d: 16,  af: 24,  k: 14.8},
+    {nominalSize: 'M20',  d: 20,  af: 30,  k: 18},
+    {nominalSize: 'M24',  d: 24,  af: 36,  k: 21.5},
+    {nominalSize: 'M30',  d: 30,  af: 46,  k: 25.6},
+    {nominalSize: 'M36',  d: 36,  af: 55,  k: 31},
+    {nominalSize: 'M42',  d: 42,  af: 65,  k: 34},
+    {nominalSize: 'M48',  d: 48,  af: 75,  k: 38},
+    {nominalSize: 'M56',  d: 56,  af: 85,  k: 45}
+  ];
+  for (var i = 0; i < metricHexNutMatrix.length; i++) {
+    metricHexNutMatrix[i].d = metricHexNutMatrix[i].d / 10;
+    metricHexNutMatrix[i].d = metricHexNutMatrix[i].d.toFixed(4);
+    metricHexNutMatrix[i].ac = (2 * metricHexNutMatrix[i].af) / Math.sqrt(3);
+    metricHexNutMatrix[i].af = metricHexNutMatrix[i].af / 10;
+    metricHexNutMatrix[i].af = metricHexNutMatrix[i].af.toFixed(4);
+    metricHexNutMatrix[i].ac = metricHexNutMatrix[i].ac / 10;
+    metricHexNutMatrix[i].ac = metricHexNutMatrix[i].ac.toFixed(4);
+    metricHexNutMatrix[i].k = metricHexNutMatrix[i].k / 10;
+    metricHexNutMatrix[i].k = metricHexNutMatrix[i].k.toFixed(4);
+  }
   var app = adsk.core.Application.get(), ui;
   if (app) {
     ui = app.userInterface;
@@ -38,7 +69,22 @@ function run(context) {
         adsk.terminate();
       });
       var inputs = command.commandInputs;
+      inputs.addStringValueInput('metricHexNutName', 'Hex Nut Name', 'Metric Hex Nut');
       inputs.addImageCommandInput("image", "", "resources/example.png");
+      var initNominalSize = inputs.addDropDownCommandInput('nominalSize', 'Nominal Size', adsk.core.DropDownStyles.TextListDropDownStyle);
+      initNominalSize.listItems.add(metricHexNutMatrix[0].nominalSize, true, '');
+      for (var i = 1; i < metricHexNutMatrix.length; i++) {
+        initNominalSize.listItems.add(metricHexNutMatrix[i].nominalSize, false, '');
+      }
+      initNominalSize.listItems.add('Custom', false, '');
+      var initD = adsk.core.ValueInput.createByReal(metricHexNutMatrix[0].d);
+      inputs.addValueInput('D', '(d) Diameter', 'cm', initD);
+      var initAf = adsk.core.ValueInput.createByReal(metricHexNutMatrix[0].af);
+      inputs.addValueInput('Af', '(af) Width Across Flats', 'cm', initAf);
+      var initWidthAc = adsk.core.ValueInput.createByReal(metricHexNutMatrix[0].ac);
+      inputs.addValueInput('Ac', '(ac) Width Across Corners', 'cm', initWidthAc);
+      var initK = adsk.core.ValueInput.createByReal(metricHexNutMatrix[0].k);
+      inputs.addValueInput('K', '(k) Thickness', 'cm', initK);
     }
     catch (e) {
       ui.messageBox('Failed to create command : ' + (e.description ? e.description : e));
